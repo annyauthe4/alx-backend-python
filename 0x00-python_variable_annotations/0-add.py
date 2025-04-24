1ac36adadb878d0bc330e5ab1325f9bf2815c9ac
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-04-24 15:54:45 +0100

    Add annotation type

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains type annotated function for basic addition.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers."""
+    return a + b

49d1bc503eb58b121eb4ef69082d3135f736403a
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-04-24 16:18:03 +0100

    Concatenate string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function that takes two string
+parameters and return a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns concatenated string."""
+    return str1 + str2

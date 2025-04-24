269cb418818d81f253e9db4a6b67b9a38dfa08bf
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-04-24 20:49:52 +0100

    Return string of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains type-annotated function which takes
+float as arg and returns the string representation of
+the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of float."""
+    return str(n)

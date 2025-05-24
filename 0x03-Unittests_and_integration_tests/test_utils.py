1cbb113ea82059845da5a4b6452e224f04b95e3e
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-05-24 03:01:40 +0100

    Test for memoize caching

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,3 +49,28 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             m_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test for memoize decorator"""
+
+    def test_memoize(self):
+        """Test that memoize caches the result of the decorated method"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mck_methd:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mck_methd.assert_called_once()

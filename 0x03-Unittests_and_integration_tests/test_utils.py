df720171d5da5043eac20f20f7feeb3d2b51e52e
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-05-24 02:41:04 +0100

    Test get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,8 +2,9 @@
 """Unit tests for utils.access_nested_map"""
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,3 +31,21 @@ class TestAccessNestedMap(unittest.TestCase):
             str(context.exception),
             repr(path[-1])
         )
+
+
+class TestGetJson(unittest.TestCase):
+    """TestCase for get_json method"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test that get_json returns the expected payload"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch("utils.requests.get", return_value=mock_response) as m_get:
+            result = get_json(test_url)
+            m_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
